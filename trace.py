6fa1b975646064af4c5312afe10826d0a89ede7c
Tanishka Sahu <140744568+Tsahu874@users.noreply.github.com>
2023-10-11 21:22:49 +0530

    trace.py

--- trace.py
+++ trace.py
@@ -1,3 +1,4 @@
+import tkinter as tk
 # Let's import the phonenumbers module to work with numbers
 import phonenumbers as pn
 
@@ -7,30 +8,46 @@ import phonenumbers as pn
 
 from phonenumbers import timezone, geocoder , carrier
 
-# any phonenumber should be in a form of string
-# +91 --->(country code) represents India
-number = input("Enter your No. with +__ : ")
+def trace_number():
+    number = entry.get()
+    # we'll use pn.parse() function
+    phone = pn.parse(number, None)
+    if pn.is_valid_number(phone):
+        # for timezone
+        time = timezone.time_zones_for_number(phone)
 
-# we'll use pn.parse() function
-phone = pn.parse(number, None)
-if pn.is_valid_number(phone):
-    # for timezone
-    time = timezone.time_zones_for_number(phone)
+        # for carrier
+        car = carrier.name_for_number(phone, "en")
 
-    # for carrier
-    car = carrier.name_for_number(phone, "en")
+        # to see where the number is registered
+        reg = geocoder.description_for_number(phone, "en")
 
-    # to see where the number is registered
-    reg = geocoder.description_for_number(phone, "en")
+        result_label.config(text =f"Phone number:{phone}\n"
+            f"Timezone: {time}\n"
+            f"Carrier: {car}\n"
+            f"Region: {reg}")
 
-    # for printing phone number and timezone
-    print(phone)
-    print(time)
-    print(car)
-    print(reg)
+   
+    else:
+        result_label.config(text="Invalid phone number.")
 
-else:
-    print("Invalid phone number.")
+root = tk.Tk()
+root.title("Contact Number Tracer")
 
-# except pn.NumberParseException:
-    # print("Invalid phone number format.")   
+label = tk.Label(root, text="Enter your phone number with +__:")
+label.pack(pady = 10)
+
+entry = tk.Entry(root, width = 50,bg ='#dbe2e9')
+entry.pack(pady=20,padx = 20)
+
+trace_button = tk.Button(root, text="Trace the number", bg= '#c8a2c8' ,command = trace_number)
+trace_button.pack(pady=20)
+
+result_label = tk.Label(root, text="")
+result_label.pack(pady=20)
+
+
+
+
+
+root.mainloop()
